30636aa03427581880c30b5bb4b2ef3c2a202e2b
Literally Void <literallyvoid@gmail.com>
2023-12-27 09:41:34 -0800

    Add World.traceRay

--- src/world.zig
+++ src/world.zig
@@ -2,8 +2,25 @@ pub const State = @import("./world/State.zig");
 const collision = @import("./collision.zig");
 const linalg = @import("./linalg.zig");
 
+pub const Hitbox = struct {
+    origin: linalg.Vec3,
+    half_extents: linalg.Vec3,
+
+    entity: ?State.EntityId,
+};
+
 pub const World = struct {
     map: *collision.BrushModel,
+
+    hitboxes: []Hitbox = &.{},
+
+    pub fn traceRay(
+        self: World,
+        origin: linalg.Vec3,
+        ray: linalg.Vec3,
+    ) ?collision.Impact {
+        return self.map.traceRay(origin, ray);
+    }
 
     pub fn traceBox(
         self: World,
